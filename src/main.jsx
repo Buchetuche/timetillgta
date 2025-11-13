0beb34078ecfc9baf72df02c32dbee29098c7a1f
Buchetuche <70153064+Buchetuche@users.noreply.github.com>
2025-11-13 18:07:07 -0500

    [FEAT] Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
+import Analytics from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Home />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
